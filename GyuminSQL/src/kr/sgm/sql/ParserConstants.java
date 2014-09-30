e76c69aa9da7c1c2c2011635a4eb28c7048cdb21
sgm <sim0629@gmail.com>
2014-09-30 11:24:01 +0900

    커맨드 라인 입력 잘안되는 버그 수정
    - ";<EOL>"로 Parse 마감
    - 출력을 Parse가 끝나고 함

--- GyuminSQL/src/kr/sgm/sql/ParserConstants.java
+++ GyuminSQL/src/kr/sgm/sql/ParserConstants.java
@@ -11,97 +11,97 @@ public interface ParserConstants {
   /** End of File. */
   int EOF = 0;
   /** RegularExpression Id. */
-  int EXIT = 3;
+  int EXIT = 5;
   /** RegularExpression Id. */
-  int SEMICOLON = 4;
+  int EOP = 6;
   /** RegularExpression Id. */
-  int EOL = 5;
+  int SEMICOLON = 7;
   /** RegularExpression Id. */
-  int CREATE = 6;
+  int CREATE = 8;
   /** RegularExpression Id. */
-  int TABLE = 7;
+  int TABLE = 9;
   /** RegularExpression Id. */
-  int NOT = 8;
+  int NOT = 10;
   /** RegularExpression Id. */
-  int NULL = 9;
+  int NULL = 11;
   /** RegularExpression Id. */
-  int PRIMARY = 10;
+  int PRIMARY = 12;
   /** RegularExpression Id. */
-  int KEY = 11;
+  int KEY = 13;
   /** RegularExpression Id. */
-  int FOREIGN = 12;
+  int FOREIGN = 14;
   /** RegularExpression Id. */
-  int REFERENCES = 13;
+  int REFERENCES = 15;
   /** RegularExpression Id. */
-  int INT = 14;
+  int INT = 16;
   /** RegularExpression Id. */
-  int CHAR = 15;
+  int CHAR = 17;
   /** RegularExpression Id. */
-  int DATE = 16;
+  int DATE = 18;
   /** RegularExpression Id. */
-  int DROP = 17;
+  int DROP = 19;
   /** RegularExpression Id. */
-  int SHOW = 18;
+  int SHOW = 20;
   /** RegularExpression Id. */
-  int TABLES = 19;
+  int TABLES = 21;
   /** RegularExpression Id. */
-  int DESC = 20;
+  int DESC = 22;
   /** RegularExpression Id. */
-  int SELECT = 21;
+  int SELECT = 23;
   /** RegularExpression Id. */
-  int AS = 22;
+  int AS = 24;
   /** RegularExpression Id. */
-  int FROM = 23;
+  int FROM = 25;
   /** RegularExpression Id. */
-  int WHERE = 24;
+  int WHERE = 26;
   /** RegularExpression Id. */
-  int OR = 25;
+  int OR = 27;
   /** RegularExpression Id. */
-  int AND = 26;
+  int AND = 28;
   /** RegularExpression Id. */
-  int IS = 27;
+  int IS = 29;
   /** RegularExpression Id. */
-  int INSERT = 28;
+  int INSERT = 30;
   /** RegularExpression Id. */
-  int INTO = 29;
+  int INTO = 31;
   /** RegularExpression Id. */
-  int VALUES = 30;
+  int VALUES = 32;
   /** RegularExpression Id. */
-  int DELETE = 31;
+  int DELETE = 33;
   /** RegularExpression Id. */
-  int LEGAL_IDENTIFIER = 32;
+  int LEGAL_IDENTIFIER = 34;
   /** RegularExpression Id. */
-  int UNDERSCORE = 33;
+  int UNDERSCORE = 35;
   /** RegularExpression Id. */
-  int LEFT_PAREN = 34;
+  int LEFT_PAREN = 36;
   /** RegularExpression Id. */
-  int RIGHT_PAREN = 35;
+  int RIGHT_PAREN = 37;
   /** RegularExpression Id. */
-  int COMMA = 36;
+  int COMMA = 38;
   /** RegularExpression Id. */
-  int STAR = 37;
+  int STAR = 39;
   /** RegularExpression Id. */
-  int PERIOD = 38;
+  int PERIOD = 40;
   /** RegularExpression Id. */
-  int COMP_OP = 39;
+  int COMP_OP = 41;
   /** RegularExpression Id. */
-  int CHAR_STRING = 40;
+  int CHAR_STRING = 42;
   /** RegularExpression Id. */
-  int INT_VALUE = 41;
+  int INT_VALUE = 43;
   /** RegularExpression Id. */
-  int DIGIT = 42;
+  int DIGIT = 44;
   /** RegularExpression Id. */
-  int SIGN = 43;
+  int SIGN = 45;
   /** RegularExpression Id. */
-  int DATE_VALUE = 44;
+  int DATE_VALUE = 46;
   /** RegularExpression Id. */
-  int NNNN = 45;
+  int NNNN = 47;
   /** RegularExpression Id. */
-  int NN = 46;
+  int NN = 48;
   /** RegularExpression Id. */
-  int QUOTE = 47;
+  int QUOTE = 49;
   /** RegularExpression Id. */
-  int ALPHABET = 48;
+  int ALPHABET = 50;
 
   /** Lexical state. */
   int DEFAULT = 0;
@@ -111,9 +111,11 @@ public interface ParserConstants {
     "<EOF>",
     "\" \"",
     "\"\\t\"",
+    "\"\\r\"",
+    "\"\\n\"",
     "\"exit\"",
+    "<EOP>",
     "\";\"",
-    "<EOL>",
     "\"create\"",
     "\"table\"",
     "\"not\"",
