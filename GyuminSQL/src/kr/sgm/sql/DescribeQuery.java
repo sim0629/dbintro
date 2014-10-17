8ca5f3303676358af082078e21e57e246e3ce04a
sgm <sim0629@gmail.com>
2014-10-18 02:07:30 +0900

    DescribeQuery.run() 구현

--- GyuminSQL/src/kr/sgm/sql/DescribeQuery.java
+++ GyuminSQL/src/kr/sgm/sql/DescribeQuery.java
@@ -1,6 +1,10 @@
 package kr.sgm.sql;
 
-class DescribeQuery extends BaseQuery {
+import java.util.ArrayList;
+
+import kr.sgm.sql.entity.*;
+
+class DescribeQuery extends BaseQuery implements IDefinitionQuery {
   @Override
   final String getTypeString() {
     return "desc";
@@ -10,5 +14,33 @@ class DescribeQuery extends BaseQuery {
 
   void setTableName(String tableName) {
     this.tableName = tableName;
+  }
+
+  @Override
+  void run() throws InvalidQueryException {
+    Table table = infoHandler.load(this.tableName);
+    if(table == null)
+      throw new InvalidQueryException(Messages.NoSuchTable);
+
+    String format = "%-23s %-16s %-4s %-7s\n";
+    System.out.println("-----------------------------------------------------");
+    System.out.printf("table_name [%s]\n", this.tableName);
+    System.out.printf(format, "column_name", "type", "null", "key");
+    ArrayList<Column> columns = table.getColumns();
+    for(Column column : columns) {
+      String columnName = column.getName();
+      if(columnName.length() > 23)
+        columnName = columnName.substring(0, 20) + "...";
+      String keyString;
+      if(column.getIsPrimaryKey()) {
+        if(column.getIsForeignKey()) keyString = "PRI,FOR";
+        else keyString = "PRI";
+      }else {
+        if(column.getIsForeignKey()) keyString = "FOR";
+        else keyString = "";
+      }
+      System.out.printf(format, columnName, column.getDataType(), column.getNullable() ? "Y" : "N", keyString);
+    }
+    System.out.println("-----------------------------------------------------");
   }
 }
