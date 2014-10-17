e7dea769581a977445a2f95219096f9607bf591d
sgm <sim0629@gmail.com>
2014-10-18 02:14:55 +0900

    ShowTablesQuery.run() 구현

--- GyuminSQL/src/kr/sgm/sql/ShowTablesQuery.java
+++ GyuminSQL/src/kr/sgm/sql/ShowTablesQuery.java
@@ -1,8 +1,22 @@
 package kr.sgm.sql;
 
-class ShowTablesQuery extends BaseQuery {
+import kr.sgm.sql.entity.*;
+
+class ShowTablesQuery extends BaseQuery implements IDefinitionQuery {
   @Override
   final String getTypeString() {
     return "show tables";
+  }
+
+  @Override
+  void run() throws InvalidQueryException {
+    System.out.println("-----------------------");
+    for(Table table : infoHandler.all()) {
+      String tableName = table.getName();
+      if(tableName.length() > 23)
+        tableName = tableName.substring(0, 20) + "...";
+      System.out.printf("%-23s\n", tableName);
+    }
+    System.out.println("-----------------------");
   }
 }
