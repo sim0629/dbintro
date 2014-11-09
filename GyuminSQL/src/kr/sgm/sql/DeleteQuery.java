3419308f56d55ff5da9b142de2af77afa02376ff
sgm <sim0629@gmail.com>
2014-11-10 01:40:09 +0900

    Delete.run 구현: 지울 것들 찾기까지

--- GyuminSQL/src/kr/sgm/sql/DeleteQuery.java
+++ GyuminSQL/src/kr/sgm/sql/DeleteQuery.java
@@ -1,5 +1,9 @@
 package kr.sgm.sql;
 
+import java.util.ArrayList;
+
+import kr.sgm.sql.entity.*;
+
 class DeleteQuery extends BaseQuery {
   @Override
   final String getTypeString() {
@@ -15,5 +19,30 @@ class DeleteQuery extends BaseQuery {
 
   void setWhere(QueryBooleanValueExpression where) {
     this.where = where;
+  }
+
+  @Override
+  void run() throws InvalidQueryException {
+    Table table = infoHandler.load(tableName);
+    if(table == null)
+      throw new InvalidQueryException(Messages.NoSuchTable);
+    ArrayList<Table> tables = new ArrayList<Table>(1);
+    tables.add(table);
+
+    QueryReferedTable qrt = new QueryReferedTable(tableName, null);
+    ArrayList<QueryReferedTable> referedTables = new ArrayList<QueryReferedTable>(1);
+    referedTables.add(qrt);
+
+    ArrayList<Record> recordsToDelete = new ArrayList<Record>();
+
+    DatabaseHandler<String, Record> tableHandler = DatabaseHandler.tableHandler(tableName);
+    for(Record record : tableHandler.all()) {
+      ArrayList<Record> records = new ArrayList<Record>(1);
+      records.add(record);
+      Boolean t = true;
+      if(where == null || t.equals(where.check(referedTables, tables, records))) {
+        recordsToDelete.add(record);
+      }
+    }
   }
 }
